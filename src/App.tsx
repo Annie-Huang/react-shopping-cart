152f10e03b3adc28f2454e8d9edce05b31fd28b0
Annie Huang <annie.hn@gmail.com>
2021-01-31 16:44:51 +1100

    Create App.App.styles.ts to use styles-components as a wrapper, and import it in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 
+// Styles
+import { Wrapper } from './App.styles';
+
 const App = () => {
   return <div className='App'>Start</div>;
 };
