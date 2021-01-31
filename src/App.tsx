11cf6bf7d2dab6894d27dd514eb82464e8a86fc0
Annie Huang <annie.hn@gmail.com>
2021-01-31 17:03:33 +1100

    Create CartItemType type and use it for getProducts call.

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,18 @@ import Badge from '@material-ui/core/Badge';
 // Styles
 import { Wrapper } from './App.styles';
 
-const getProducts = async () =>
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType> =>
   await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
