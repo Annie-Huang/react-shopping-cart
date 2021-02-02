dd656d3761300e621fb7017182d9026d81479d36
Annie Huang <annie.hn@gmail.com>
2021-02-02 21:37:00 +1100

    Styled the Item.styles.ts

--- src/Item/Item.tsx
+++ src/Item/Item.tsx
@@ -15,7 +15,7 @@ const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
   <Wrapper>
     <img src={item.image} alt={item.title} />
     <div>
-      <h3>{item.image}</h3>
+      <h3>{item.title}</h3>
       <p>{item.description}</p>
       <h3>${item.price}</h3>
     </div>
