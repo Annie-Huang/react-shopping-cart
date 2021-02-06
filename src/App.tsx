2e788fb357ae861d0f420cf1eb5589542dfa2d41
Annie Huang <annie.hn@gmail.com>
2021-02-06 13:23:51 +1100

    Implement the handleRemoveFromCart function

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,18 @@ const App = () => {
     });
   };
 
-  const handleRemoveFromCart = () => null;
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems((prev) =>
+      prev.reduce((accumulator, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return accumulator;
+          return [...accumulator, { ...item, amount: item.amount - 1 }];
+        }
+
+        return [...accumulator, item];
+      }, [] as CartItemType[])
+    );
+  };
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Something went wrong ...</div>;
