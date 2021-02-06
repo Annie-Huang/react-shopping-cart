5208ed68beb683a8a4e1f4229261c570c6742469
Annie Huang <annie.hn@gmail.com>
2021-02-06 12:02:31 +1100

    Add <Cart> into App.tsx and confirm we can see 'No items in cart' when we clicked into the cart component

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
+import Cart from './Cart/Cart';
 
 // Styles
 import { Wrapper, StyledButton } from './App.styles';
@@ -49,7 +50,11 @@ const App = () => {
   return (
     <Wrapper>
       <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
-        Cart goes here.
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
       </Drawer>
       <StyledButton onClick={() => setCartOpen(true)}>
         <Badge badgeContent={getTotalItems(cartItems)} color='error'>
